9c7bfd53317af485eb24dc35f0f975b0f39b9830
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-06-05 13:19:04 +0530

    Tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 mod state;
+
+use tokio::net::TcpListener;
+
 use state::{User,Room,ChatAppRoom};
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() ->Result<(),Box<dyn std::error::Error>>{
+    let listener=TcpListener::bind("127.0.0.1:8080").await?;
+    println!("server is live and listening to 127.0.0.1:8080");
+    loop{
+        let (socket,addr)=listener.accept().await?;
+        println!("new connection established from {}",addr);
+        tokio::spawn(async move {
+            handle_connection(socket).await;
+
+        });
+    }
+}
+
+async fn handle_connection(socket:tokio::net::TcpStream){
+    println!("communicating with client");
 }
---
+++ target/debug/deps/multi_room_websocket_chat_server-b8d29652c755febd.d
@@ -0,0 +1,6 @@
+C:\Users\sathw\Documents\rs+sol+zkp\Rust-Projects\multi-room-websocket-chat-server\target\debug\deps\multi_room_websocket_chat_server-b8d29652c755febd.d: src\main.rs src\state.rs
+
+C:\Users\sathw\Documents\rs+sol+zkp\Rust-Projects\multi-room-websocket-chat-server\target\debug\deps\libmulti_room_websocket_chat_server-b8d29652c755febd.rmeta: src\main.rs src\state.rs
+
+src\main.rs:
+src\state.rs:
